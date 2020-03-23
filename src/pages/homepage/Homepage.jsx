0636324016c2ae6962819d7344c661223644da9e
Harshit-Dandriyal <dandriyalh9318@gmail.com>
2020-03-23 17:21:53 +0530

    Beginning

---
+++ src/pages/homepage/Homepage.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import Directory from '../../Component/directory/directory';
+
+import './HomepageStyles.scss';
+
+const HomePage = () => (
+  <div className='homepage'>
+    <Directory />
+  </div>
+);
+
+export default HomePage;
